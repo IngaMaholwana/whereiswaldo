9fc90c85b54ae2d45a5ae90dd39c2290d159b878
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-18 17:48:34 +0200

    waldo backgrpound

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
+import React from 'react';
+import BackgroundImg from ' ./src/component/BackgroundImg';
+import bgImage from './assets/waldo.jpg'; // make sure you have a file here
 
-import './App.css'
+const App = () => {
+  const handleChange = (e) => {
+    console.log("Image clicked at:", e.clientX, e.clientY);
+  };
 
-const App = () => <h1>Our First Test</h1>;
+  return (
+    <div className="App">
+      <h1>Our First Test</h1>
+      <BackgroundImg
+        src={bgImage}
+        setImgSize={() => {}}
+        setXY={() => {}}
+        handleChange={handleChange}
+        correctCoords={{ x: 0, y: 0 }} // dummy value for now
+      />
+    </div>
+  );
+};
 
 export default App;
+// import './App.css'
+
+// const App = () => <h1>Our First Test</h1>;
+
+// export default App;
 
 // function App() {
   
